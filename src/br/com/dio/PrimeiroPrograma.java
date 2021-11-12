f5a8125df7975370aea5d62379ef695e1c55a67f
MarcelloAllves <marcello.allves@hotmail.com>
2021-11-12 08:21:41 -0300

    criando a class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -2,9 +2,12 @@ package br.com.dio;
 
 public class PrimeiroPrograma {
     public static void main(String[] args) {
-        Gato gato = new Gato();
-        System.out.println(gato);
-
+       /*
+       Gato gato = new Gato();
+       System.out.println(gato);
+       */
+        Livro livro = new Livro("nome do livro", 300);
+        System.out.println(livro);
         /*
         int a = 5;
         int b = 3;
@@ -13,5 +16,31 @@ public class PrimeiroPrograma {
         */
 
 
+    }
+
+    static class Livro{
+        private String nome;
+        private int numPaginas;
+
+        public Livro(String nome, int numPaginas) {
+            this.nome = nome;
+            this.numPaginas = numPaginas;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public int getNumPaginas() {
+            return numPaginas;
+        }
+
+        public void setNumPaginas(int numPaginas) {
+            this.numPaginas = numPaginas;
+        }
     }
 }
